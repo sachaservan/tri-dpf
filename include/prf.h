ab5cb157e6bffe86bf78e41fbda7354a58a92afb
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2023-12-13 11:48:20 +0100

    Make AES calls a static inline function

--- include/prf.h
+++ include/prf.h
@@ -9,7 +9,27 @@ typedef unsigned __int128 uint128_t;
 
 EVP_CIPHER_CTX *PRFKeyGen(uint8_t *key);
 void DestroyPRFKey(EVP_CIPHER_CTX *ctx);
-void PRFEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs);
-void PRFBatchEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs, int num_blocks);
+
+// void PRFEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs);
+// void PRFBatchEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs, int num_blocks);
+
+static inline void PRFEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs)
+{
+    int len = 0;
+    if (1 != EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16))
+        printf("errors ocurred in PRF evaluation\n");
+}
+
+// PRF used to expand the DPF tree. Just a call to AES-ECB.
+// Note: we use ECB-mode (instead of CTR) as we want to manage each block separately.
+static inline void PRFBatchEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs, int num_blocks)
+{
+    static int len = 0; // make static to avoid reallocating
+    EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16 * num_blocks);
+
+    // DEBUG
+    // if (1 != EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16 * num_blocks))
+    //     printf("errors ocurred in PRF evaluation\n");
+}
 
 #endif
--- src/prf.c
+++ src/prf.c
@@ -25,22 +25,3 @@ void DestroyPRFKey(EVP_CIPHER_CTX *ctx)
 {
     EVP_CIPHER_CTX_free(ctx);
 }
-
-inline void PRFEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs)
-{
-    int len = 0;
-    if (1 != EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16))
-        printf("errors ocurred in PRF evaluation\n");
-}
-
-// PRF used to expand the DPF tree. Just a call to AES-ECB.
-// Note: we use ECB-mode (instead of CTR) as we want to manage each block separately.
-inline void PRFBatchEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs, int num_blocks)
-{
-    static int len = 0; // make static to avoid reallocating
-    EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16 * num_blocks);
-
-    // DEBUG
-    // if (1 != EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16 * num_blocks))
-    //     printf("errors ocurred in PRF evaluation\n");
-}
