fe78852a55747f315dcf342bc470e73116da57b5
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2024-01-30 11:41:27 +0100

    Use Davies–Meyer for PRF

--- include/prf.h
+++ include/prf.h
@@ -10,22 +10,26 @@ typedef unsigned __int128 uint128_t;
 EVP_CIPHER_CTX *PRFKeyGen(uint8_t *key);
 void DestroyPRFKey(EVP_CIPHER_CTX *ctx);
 
-// void PRFEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs);
-// void PRFBatchEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs, int num_blocks);
-
+// XOR with input to prevent inversion using Davies–Meyer construction
 static inline void PRFEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs)
 {
     int len = 0;
     if (1 != EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16))
         printf("errors ocurred in PRF evaluation\n");
+
+    outputs[0] ^= input[0];
 }
 
 // PRF used to expand the DPF tree. Just a call to AES-ECB.
 // Note: we use ECB-mode (instead of CTR) as we want to manage each block separately.
+// XOR with input to prevent inversion using Davies–Meyer construction
 static inline void PRFBatchEval(EVP_CIPHER_CTX *ctx, uint128_t *input, uint128_t *outputs, int num_blocks)
 {
     static int len = 0; // make static to avoid reallocating
     EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16 * num_blocks);
+
+    for (size_t i = 0; i < num_blocks; i++)
+        outputs[i] ^= input[i];
 
     // DEBUG
     // if (1 != EVP_EncryptUpdate(ctx, (uint8_t *)outputs, &len, (uint8_t *)input, 16 * num_blocks))
